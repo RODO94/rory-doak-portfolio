8ae3c9535d01dd4290323e4b1fa979847e64d2f7
Rory Doak <rory.doak@gmail.com>
2024-01-08 15:09:29 +0000

    remove about on header and add link for Contact

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,12 +6,16 @@ export default function Header() {
   return (
     <header className="header">
       <nav className="header-nav">
-        <a className="header-nav__link">About</a>
         <a className="header-nav__link">Projects</a>
         <a className="header-nav__link">Experience</a>
       </nav>
       <div className="header__contact">
-        <a className="header__contact-txt">Contact</a>
+        <a
+          className="header__contact-txt"
+          href="https://www.linkedin.com/in/rory-doak/"
+        >
+          Contact
+        </a>
       </div>
       <div className="header__icons">
         <a className="header__anchor" href="https://github.com/RODO94">
